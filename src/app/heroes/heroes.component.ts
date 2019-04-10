b47be6e74a1b7e74d12ea3804d7c191d2a2b8865
Lauren Judge <l.m.judge@hotmail.com>
2019-04-10 16:33:13 +1200

    Heroes component updated - retrieves data from service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -15,15 +15,19 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+  this.heroes = this.heroService.getHeroes();
+}
 
-  constructor() { }
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
   }
